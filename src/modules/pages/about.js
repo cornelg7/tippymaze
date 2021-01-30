c7991077b84033b585ff81536f5f958000df9601
Cornel <cornelg7@yahoo.com>
2021-01-30 21:27:00 +0200

    Last update on this framework

--- src/modules/pages/about.js
+++ src/modules/pages/about.js
@@ -5,7 +5,7 @@ export default class About {
     this.pageName = 'about';
     this.content = `
     <div class="${styles.row}">
-      <h3 class="${styles['u-size-medium']}">
+      <h3 class="${styles['about-h3']} ${styles['u-size-medium']}">
         Game made by cornelg7 using
         <a href="https://atomiks.github.io/tippyjs/" target="_blank">
           tippyjs
@@ -13,7 +13,7 @@ export default class About {
       </h3>
     </div>
     <div class="${styles.row}">
-      <h3 class="${styles['u-size-medium']}">
+      <h3 class="${styles['about-h3']} ${styles['u-size-medium']}">
         More details
         <a href="https://github.com/cornelg7/tippymaze" target="_blank">
           here
